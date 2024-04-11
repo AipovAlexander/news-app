e4baea2f23009ceedaf23d3cd9bcbf09c106e2b0
Alexander Libaur <libaur.alexander@gmail.com>
2024-04-11 12:10:11 +0300

    build: install and configure Chakra UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,11 @@
+import { ChakraProvider } from "@chakra-ui/react";
+
 function App() {
-  return <>Hello World!</>;
+  return (
+    <ChakraProvider>
+      <>Hello World!</>
+    </ChakraProvider>
+  );
 }
 
 export default App;
